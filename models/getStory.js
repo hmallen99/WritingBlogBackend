2645f794fc4cc59bb6fbc01f22ca276a6a01d996
hmallen99 <31718268+hmallen99@users.noreply.github.com>
2020-08-14 17:54:00 -0400

    updated getSTory to receive and send to frontend

--- models/getStory.js
+++ models/getStory.js
@@ -1,6 +1,9 @@
 function getStory(req, res) {
+    var name = req.body.name;
     var result = {
-        response : "If you are receivng this, then the backend is working"
+        title : name,
+        author: "Henry Allen",
+        body: "lorem ipsumasf jkajsd;lfkja;sd jdkljg;askdjf ",
     };
     return res.send(JSON.stringify(result));
 }
